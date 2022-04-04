51ca2716689cf79ea2d8db37d6eae1aabd926678
jwillems-snv <82822930+jwillems-snv@users.noreply.github.com>
2022-04-04 20:47:55 +0200

    Added comments and source

--- pairs_python.py
+++ pairs_python.py
@@ -1,7 +1,7 @@
 
 import itertools as it
 
-teams = ['Marketing', 'PO', 'DA', 'Tech', 'CCC', 'BusDev']
+teams = ['Marketing', 'PO', 'DA', 'Tech', 'CCC', 'BusDev', 'HR', 'Finance'] 
 
 print('All combinations:')
 
@@ -11,21 +11,26 @@ for x,y in it.combinations(teams, 2):
 print('\n'* 2)
 
 
-
 ######################################
 
-n = int(len(teams)/2)                   #number of combinations within one round, 6 teams --> 6/2 = 3 combinations
+##https://en.wikipedia.org/wiki/Round-robin_tournament#Scheduling_algorithm
+
+
+n = int(len(teams)/2)       #number of combinations within one round, 6 teams --> 6/2 = 3 combinations
 
 groups = []
 
 print('Rounds of 3:')
 
-for i in range(len(teams)-1):           #number of rounds equals number of teams minus 1, 6 teams --> 5 rounds
-    t = teams[:1] + teams[-i:] + teams [1:-i] if i else teams           #t = first item in list + last i teams + all teams except last i teams 
-    groups.append(list(zip(t[:n], reversed(t[n:]))))
+for i in range(len(teams)-1):                                       #number of rounds equals number of teams minus 1, 6 teams --> 5 rounds
+    rt = teams[:1] + teams[-i:] + teams [1:-i] if i else teams      #rt --> list with rotating teams --> first item in teams list (Marketing) + last i items in teams + items in teams between first and last i teams (i.e. Teams in between)
+    groups.append(list(zip(rt[:n], reversed(rt[n:]))))              #append to groups list --> first items of rt and rt reversed
 
 count = 1
 
 for i in groups:
     print(f'Round {count}', i, '\n')
     count = count+1
+
+
+    
